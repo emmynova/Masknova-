53b1e235330d97f86cb69988c9f74ac08d396561
emmynova <queen1112e@gmail.com>
2025-08-27 17:20:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-﻿// Wait 3 seconds then hide splash and show sign in
-setTimeout(() => {
-  document.getElementById("splash").style.display = "none";
-  document.getElementById("signin").classList.remove("hidden");
-}, 3000);
